b7721700c44f46bec4a62c1efafb295379ee4c82
Michael Schumacher <mschu528@gmail.com>
2016-10-05 08:36:10 -0500

    Added comments to display shell scripts

--- normalDisplay.sh
+++ normalDisplay.sh
@@ -1,2 +1,8 @@
+#
+# This script finds any PIDs associated with x11vnc (except the grep
+# search itself), and kills them. Then it sets the output on eDP1 to
+# 1920x1080 resolution
+#
+
 ps -ef | grep x11vnc | grep -v grep | awk '{print $2}' | sudo xargs kill
 xrandr --fb 1920x1080 --output eDP1 --panning 1920x1080+0+0/1920x1080+0+0
